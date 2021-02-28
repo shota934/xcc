5863510684625e9ce0e32477c4583bd3abd9f9c7
shota934 <shisai934@gmail.com>
2021-02-28 21:42:42 +0900

    Add the renamed variables.

--- src/gen.c
+++ src/gen.c
@@ -8,23 +8,43 @@
 #include "mem.h"
 
 gen_info_t *create_gen_info(){
-  
-  return NULL;
+
+  gen_info_t *gi;
+#ifdef __DEBUG__
+  printf("create_gen_info\n");
+#endif
+
+  gi = mem(sizeof(gen_info_t));
+  gi->stack_pos = 0;
+  gi->offset = 0;
+
+  return gi;
 }
 
-void init_gen_info(gen_info_t *ei){
-  
+void init_gen_info(gen_info_t *gi){
+
+#ifdef __DEBUG__
+  printf("init_gen_info\n");
+#endif
+  gi->stack_pos = 0;
+  gi->offset = 0;
+
   return;
 }
 
-list_t *gen(gen_info_t *ei,env_t *env,list_t *lst){
-  
-  return NULL;
+list_t *gen(gen_info_t *gi,env_t *env,list_t *lst){
+
+  list_t *val;
+#ifdef __DEBUG__
+  printf("gen\n");
+#endif
+
+  return val;
 }
 
-void delete_gen_info(gen_info_t *ei){
+void delete_gen_info(gen_info_t *gi){
 
-  fre(ei);
+  fre(gi);
 
   return;
 }
--- src/gen.h
+++ src/gen.h
@@ -9,14 +9,15 @@
 #include "env.h"
 
 typedef struct gen_info_t gen_info_t;
-#define GEN_INF_SET_FILE(ei,f)   ei->output_file = f
+#define GEN_INF_SET_FILE(gi,f)   gi->output_file = f
 struct gen_info_t {
   file_t *output_file;
   int stack_pos;
+  int offset;
 };
 
 gen_info_t *create_gen_info();
-list_t *gen(gen_info_t *ei,env_t *env,list_t *lst);
-void delete_gen_info(gen_info_t *ei);
+list_t *gen(gen_info_t *gi,env_t *env,list_t *lst);
+void delete_gen_info(gen_info_t *gi);
 
 #endif
