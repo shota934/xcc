a0b443453bdeacf07cd91e78c5dcbb5e8fba0dc0
shota934 <shisai934@gmail.com>
2021-03-07 23:33:25 +0900

    Add assigning integer value.

--- src/gen.c
+++ src/gen.c
@@ -11,6 +11,16 @@
 
 char const *REGS[] = {"rdi","rsi","rdx","rcx","r8","r9"};
 
+#define AL  "al"
+#define AX  "ax"
+#define EAX "eax"
+#define RAX "rax"
+
+#define MOVB "movb"
+#define MOVW "movw"
+#define MOVL "movl"
+#define MOVQ "movq"
+
 #define SIZE sizeof(void *)
 #define EMIT_NO_INDENT(ei,...) emitf(ei,0x00,__VA_ARGS__)
 #define EMIT(ei,...)        emitf(ei,__LINE__, "\t" __VA_ARGS__)
@@ -26,10 +36,14 @@ static bool_t has_args(list_t *lst);
 
 static void emitf(gen_info_t *ei,int line, char *fmt, ...);
 
+static void gen_info_add_offset(gen_info_t *gi,int offset);
+static int gen_info_get_offset(gen_info_t *gi);
+static void gen_info_add_localarea(gen_info_t *gi,int localarea);
 static list_t *gen_funcdef(gen_info_t *gi,env_t *env,list_t *lst);
 static list_t *gen_func_name(gen_info_t *gi,env_t *env,list_t *lst);
 static list_t *gen_function(gen_info_t *gi,env_t *env,list_t *lst);
 static list_t *gen_body(gen_info_t *gi,env_t *env,list_t *lst);
+static list_t *gen_oprand(gen_info_t *gi,env_t *env,list_t *lst);
 static list_t *gen_integer(gen_info_t *gi,env_t *env,list_t *lst);
 static list_t *gen_ret(gen_info_t *gi);
 static list_t *gen_args(gen_info_t *gi,env_t *env,list_t *lst);
@@ -37,17 +51,22 @@ static void gen_local_vars(gen_info_t *gi,env_t *env,list_t *lst);
 static int gen_loacl_ints(gen_info_t *gi,env_t *env,list_t *lst);
 static int gen_loacl_int(gen_info_t *gi,env_t *env,list_t *lst);
 
+static symbol_t *factory_symbol(gen_info_t *gi,list_t *lst);
 
 static list_t *gen_symbol(gen_info_t *gi,env_t *env,list_t *lst);
 static list_t *gen_decl_var(gen_info_t *gi,env_t *env,list_t *lst);
 static list_t *gen_return(gen_info_t *gi,env_t *env,list_t *lst);
+static list_t *gen_assign(gen_info_t *gi,env_t *env,list_t *lst);
+static list_t *lookup_symbol(env_t *env,string_t name);
 static list_t *gen_func_parms(gen_info_t *gi,env_t *env,list_t *lst);
-static list_t *gen_func_parm(gen_info_t *gi,env_t *env,list_t *lst,int index);
+static int gen_func_parm(gen_info_t *gi,env_t *env,list_t *lst);
 static symbol_t *gen_func_parm_primitive(gen_info_t *gi,env_t *env,list_t *lst);
 static void push(gen_info_t *gi,string_t reg);
 static void pop(gen_info_t *gi,string_t reg);
 
 static integer_t select_size(list_t *lst);
+static string_t select_reg(integer_t size);
+static string_t select_inst(integer_t size);
 
 gen_info_t *create_gen_info(){
 
@@ -71,6 +90,7 @@ void init_gen_info(gen_info_t *gi){
 #endif
   gi->stack_pos = 0;
   gi->offset = 0;
+  gi->localarea = 0;
 
   return;
 }
@@ -142,6 +162,24 @@ static void emitf(gen_info_t *gi,int line, char *fmt, ...){
   va_end(args);
 
   fprintf(GET_OUTPUT_FILE(gi),"\n");
+
+  return;
+}
+
+static void gen_info_add_offset(gen_info_t *gi,int offset){
+
+  gi->offset += offset;
+
+  return;
+}
+
+static int gen_info_get_offset(gen_info_t *gi){
+  return gi->offset;
+}
+
+static void gen_info_add_localarea(gen_info_t *gi,int localarea){
+
+  gi->localarea += localarea;
 
   return;
 }
@@ -246,6 +284,37 @@ static list_t *gen_body(gen_info_t *gi,env_t *env,list_t *lst){
   return val;
 }
 
+static list_t *gen_operand(gen_info_t *gi,env_t *env,list_t *lst){
+
+  list_t *val;
+  list_type_t type;
+
+#ifdef __DEBUG__
+  printf("gen_operand\n");
+#endif
+
+  val = make_null();
+  DUMP_AST(lst);
+  type = LIST_GET_TYPE(lst);
+  switch(type){
+  case LIST:
+	val = gen_operand(gi,env,car(lst));
+	break;
+  case INTEGER:
+	val = gen_integer(gi,env,lst);
+	break;
+  case SYMBOL:
+	val = gen_symbol(gi,env,lst);
+	break;
+  case NULL_LIST:
+	break;
+  default:
+	break;
+  }
+
+  return val;
+}
+
 static list_t *gen_integer(gen_info_t *gi,env_t *env,list_t *lst){
 
   list_t *val;
@@ -307,7 +376,7 @@ static void gen_local_vars(gen_info_t *gi,env_t *env,list_t *lst){
   dump_ast(type_lst);
   localarea += gen_loacl_ints(gi,env,lst);
 
-  gi->localarea = localarea;
+  gen_info_add_localarea(gi,localarea);
 
   return;
 }
@@ -342,6 +411,29 @@ static int gen_loacl_int(gen_info_t *gi,env_t *env,list_t *lst){
   return select_size(DECL_TYPE_GET_TYPE(lst));
 }
 
+static symbol_t *factory_symbol(gen_info_t *gi,list_t *lst){
+
+  symbol_t *sym;
+  int size;
+  int offset;
+
+#ifdef __DEBUG__
+  printf("factory_symbol\n");
+#endif
+
+  dump_ast(lst);
+  sym = create_symbol();
+  size = select_size(lst);
+  offset = gen_info_get_offset(gi);
+  offset -= size;
+  gen_info_add_offset(gi,size);
+  SYMBOL_SET_SIZE(sym,size);
+  SYMBOL_SET_OFFSET(sym,offset);
+  SYMBOL_SET_TYPE_LST(sym,lst);
+
+  return sym;
+}
+
 static list_t *gen_symbol(gen_info_t *gi,env_t *env,list_t *lst){
 
   string_t symbol;
@@ -355,9 +447,11 @@ static list_t *gen_symbol(gen_info_t *gi,env_t *env,list_t *lst){
   if(STRCMP(RETURN,symbol)){
 	val = gen_return(gi,env,cdr(lst));
   } else if(STRCMP(DECL_VAR,symbol)){
-	val = gen_decl_var(gi,env,cdr(lst));
+	val = gen_decl_var(gi,env,car(cdr(lst)));
+  } else if(STRCMP(ASSIGN,symbol)){
+	val = gen_assign(gi,env,cdr(lst));
   } else {
-
+	val = lookup_symbol(env,symbol);
   }
 
   return val;
@@ -367,6 +461,7 @@ static list_t *gen_decl_var(gen_info_t *gi,env_t *env,list_t *lst){
 
   list_t *val;
   symbol_t *symbol;
+  string_t name;
 
 #ifdef __DEBUG__
   printf("gen_decl_var\n");
@@ -374,7 +469,54 @@ static list_t *gen_decl_var(gen_info_t *gi,env_t *env,list_t *lst){
 
   dump_ast(lst);
 
+  name = car(cdr(lst));
+  symbol = factory_symbol(gi,car(lst));
+  insert_obj(env,name,symbol);
+
   return val;
+}
+
+static list_t *gen_assign(gen_info_t *gi,env_t *env,list_t *lst){
+
+  list_t *r;
+  list_t *l;
+  symbol_t *sym;
+  string_t op;
+  string_t reg;
+
+#ifdef __DEBUG__
+  printf("gen_assign\n");
+#endif
+
+  // LHS
+  l = gen_operand(gi,env,lst);
+  sym = (symbol_t *)car(l);
+
+  // RHS
+  r = gen_operand(gi,env,cdr(lst));
+
+  op = select_inst(SYMBOL_GET_SIZE(sym));
+  reg = select_reg(SYMBOL_GET_SIZE(sym));
+
+  EMIT(gi,"%s #%s,%d(#rbp)",op,reg,SYMBOL_GET_OFFSET(sym));
+
+  return make_null();
+}
+
+static list_t *lookup_symbol(env_t *env,string_t name){
+
+  symbol_t *sym;
+
+#ifdef __DEBUG__
+  printf("lookup_symbol\n");
+#endif
+
+  sym = lookup_obj(env,name);
+  if(!sym){
+	exit(1);
+  }
+
+  return cons(make_null(),sym);
 }
 
 static list_t *gen_return(gen_info_t *gi,env_t *env,list_t *lst){
@@ -395,41 +537,30 @@ static list_t *gen_func_parms(gen_info_t *gi,env_t *env,list_t *lst){
 
   list_t *val;
   list_t *p;
-  int i;
+  int localarea;
 
 #ifdef __DEBUG__
   printf("gen_func_parms\n");
 #endif
 
   val = make_null();
-  i = 0;
+  localarea = 0;
   for(p = lst; IS_NOT_NULL_LIST(p); p = cdr(p)){
-	gen_func_parm(gi,env,car(p),i);
-	if(i < sizeof(REGS) / sizeof(REGS[0])){
-	  push(gi,(string_t)REGS[i]);
-	}
-	i++;
+	localarea += gen_func_parm(gi,env,car(p));
   }
+
+  gen_info_add_localarea(gi,localarea);
 
   return val;
 }
 
-static list_t *gen_func_parm(gen_info_t *gi,env_t *env,list_t *lst,int index){
-
-  list_t *name;
-  list_t *type_lst;
-  list_t *val;
+static int gen_func_parm(gen_info_t *gi,env_t *env,list_t *lst){
 
 #ifdef __DEBUG__
   printf("gen_func_parm\n");
 #endif
 
-  val = make_null();
-  type_lst = car(lst);
-
-  name = cdr(lst);
-
-  return val;
+  return select_size(tail(car(lst)));
 }
 
 static symbol_t *gen_func_parm_primitive(gen_info_t *gi,env_t *env,list_t *lst){
@@ -469,11 +600,11 @@ static integer_t select_size(list_t *lst){
 #ifdef __DEBUG__
   printf("select_size");
 #endif
-  if(STRCMP(car(lst),"*")){
 
+  if(STRCMP(car(lst),"*")){
+	return SIZE;
   }
 
-  dump_ast(lst);
   type = tail(lst);
   if(STRCMP(car(type),INT)){
 	size = sizeof(int);
@@ -492,4 +623,52 @@ static integer_t select_size(list_t *lst){
   }
 
   return size;
+}
+
+static string_t select_reg(integer_t size){
+
+  string_t op;
+#ifdef __DEBUG__
+  printf("select_reg\n");
+#endif
+
+  switch(size){
+  case 1:
+	op = "al";
+	break;
+  case 2:
+	op = "ax";
+  case 4:
+	op = "eax";
+	break;
+  case 8:
+	op = "rax";
+	break;
+  }
+
+  return op;
+}
+
+static string_t select_inst(integer_t size){
+
+  string_t op;
+#ifdef __DEBUG__
+  printf("select_inst\n");
+#endif
+
+  switch(size){
+  case 1:
+	op = "movb";
+	break;
+  case 2:
+	op = "movw";
+  case 4:
+	op = "movl";
+	break;
+  case 8:
+	op = "movq";
+	break;
+  }
+
+  return op;
 }
