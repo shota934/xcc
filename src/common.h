076848a2860c011230d22057a306ee51ce0d254f
shota934 <shisai934@gmail.com>
2021-06-27 18:36:17 +0900

    Add the fixed common header.

--- src/common.h
+++ src/common.h
@@ -13,7 +13,8 @@
 #include "dump.h"
 
 #define __LINUX__
-#define __DEBUG__
+//
+//#define __DEBUG__
 //#define __CPP__DEBUG__
 
 #define XCC_VERSION "0.1"
@@ -36,11 +37,13 @@
 #define DEFAULT_OUTPUT_FILE "tmp.s"
 #define END_OF_CHAR 0x00
 #define TRUE_OBJ            "1"
+#define SIZE sizeof(void *)
 
-#define INCLUDE_PATH             "/usr/include/"
-#define INCLUDE_LINUX_PATH       "/usr/include/linux/"
-#define INCLUDE_GNU_LINUX_PATH   "/usr/include/x86_64-linux-gnu/"
-#define INCLUDE_XCC              "include/"
+#define INCLUDE_PATH               "/usr/include/"
+#define INCLUDE_LINUX_PATH         "/usr/include/linux/"
+#define INCLUDE_GNU_LINUX_PATH     "/usr/include/x86_64-linux-gnu/"
+#define INCLUDE_GNU_LINUX_LIB_PATH "/usr/lib/gcc/x86_64-linux-gnu/9/include/"
+#define INCLUDE_XCC                "include/"
 
 #define STRCMP(s1,s2) (strcmp(s1,s2) == 0)
 #define STRLEN(s) (int)strlen(s);
