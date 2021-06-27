73b7ea4faebb716a34a056985baef04f26cc8e1b
shota934 <shisai934@gmail.com>
2021-06-27 15:08:30 +0900

    Add two header files. The file added as follows.
     - xcc
     - stddef

---
+++ src/include/xcc.h
@@ -0,0 +1,16 @@
+#ifndef __XCC__
+#define __XCC__
+
+#define __alignof__ alignof
+#define __const__ const
+#define __inline__ inline
+#define __restrict__ restrict
+#define __restrict restrict
+#define __signed__ signed
+#define __typeof__ typeof
+#define __volatile__ volatile
+
+typedef unsigned short char16_t;
+typedef unsigned int char32_t;
+
+#endif
