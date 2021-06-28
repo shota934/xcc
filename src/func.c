95985ad586dc6bf63b9d671607c6cbcf4d518082
shota934 <shisai934@gmail.com>
2021-06-28 09:32:40 +0900

    Add the source files for function object.

--- src/func.c
+++ src/func.c
@@ -2,15 +2,16 @@
 #include "mem.h"
 #include "type.h"
 
-func_t *create_func(list_t *args,list_t *ret_type){
+func_t *create_func(list_t *args,list_t *ret_type,scope_t scope){
 
   func_t *func;
 
   func = mem(sizeof(func_t));
-  func->obj_type.type = TYPE_FUNCTION;
+  func->obj.type = TYPE_FUNCTION;
+  func->obj.scope = scope;
   func->args = args;
   func->ret_type = ret_type;
   func->len = length_of_list(args);
-  
+
   return func;
 }
--- src/func.h
+++ src/func.h
@@ -4,13 +4,15 @@
 #include "obj.h"
 #include "list.h"
 
+#define FUNC_GET_RET_TYPE(f) f->ret_type
+
 struct func_t {
-  object_t obj_type;
+  object_t obj;
   list_t *args;
-  int len;
   list_t *ret_type;
+  int len;
 };
 
-func_t *create_func();
+func_t *create_func(list_t *args,list_t *ret_type,scope_t scope);
 
 #endif
