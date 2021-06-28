f62ea68df28964f7fb32ec682f556a1e3bf06b1d
shota934 <shisai934@gmail.com>
2021-06-28 13:36:38 +0900

    Add the fixed source files for the symbol objects.

--- src/symbol.c
+++ src/symbol.c
@@ -7,19 +7,47 @@
 #include "mem.h"
 #include "common.h"
 #include "list.h"
+#include "dump.h"
 
-symbol_t *create_symbol(){
+#define FUNC "FUNC"
+
+static list_t *get_ret_type(list_t *lst);
+symbol_t *create_symbol(list_t *lst){
 
   symbol_t *sym;
   
   sym = mem(sizeof(symbol_t));
-  sym->obj_type.type = TYPE_SYMBOL;
-  sym->scope = UNDEFINED;
+  sym->obj.type = TYPE_SYMBOL;
+  sym->obj.scope = UNDEFINED;
   sym->size = 0;
   sym->offset = 0;
-  sym->type_lst = make_null();
+  sym->type_lst = lst;
+  sym->func.ret_type = get_ret_type(lst);
+
+  DUMP_AST(sym->type_lst);
   
   return sym;
 }
 
+static list_t *get_ret_type(list_t *lst){
 
+  list_t *p;
+  string_t name;
+
+#ifdef __DEBUG__
+  printf("get_ret_type\n");
+#endif
+
+  p = lst;
+  while(IS_NOT_NULL_LIST(p)){
+	if(IS_SYMBOL(p)){
+	  name = (string_t )car(p);
+	  if(STRCMP(name,FUNC)){
+		return cdr(cdr(p));
+	  }
+	}
+	p = cdr(p);
+  }
+
+  return make_null();
+}
--- src/symbol.h
+++ src/symbol.h
@@ -7,30 +7,37 @@
 #define __INCLUDE__SYMBOL__
 #include "type.h"
 #include "obj.h"
+#include "env.h"
 
-#define SYMBOL_SET_VAR_TYPE(s,t) s->var_type = t
 #define SYMBOL_SET_TYPE(s,t) s->type = t
 #define SYMBOL_SET_OFFSET(s,o) s->offset = o
 #define SYMBOL_SET_SIZE(s,si)  s->size = si
 #define SYMBOL_SET_TYPE_LST(s,l) s->type_lst = l
-#define SYMBOL_SET_SCOPE(s,sc) s->scope = sc
+#define SYMBOL_SET_SCOPE(s,sc) s->obj.scope = sc
 
-#define SYMBOL_GET_VAR_TYPE(s)  s->var_type
-#define SYMBOL_GET_TYPE(s)      s->type
 #define SYMBOL_GET_OFFSET(s)    s->offset
 #define SYMBOL_GET_SIZE(s)      s->size
+
+#define SYMBOL_GET_TYPE(s)  s->obj.type
 #define SYMBOL_GET_TYPE_LST(s)  s->type_lst
-#define SYMBOL_GET_SCOPE(s)     s->scope
+#define SYMBOL_GET_SCOPE(s) s->obj.scope
+#define SYMBOL_GET_RET_LST(s) s->func.ret_type
+#define SYMBOL_GET_SYM_TYPE(s) s->type
 
 struct symbol_t {
-  object_t obj_type;
+  object_t obj;
   list_t *type_lst;
-  scope_t scope;
   type_t type;
   int offset;
   int size;
+  env_t *env;
+
+  union {
+	list_t *ret_type;
+  } func;
 };
 
-symbol_t *create_symbol();
+symbol_t *create_symbol(list_t *lst);
+static list_t *get_ret_type(list_t *lst);
 
 #endif
